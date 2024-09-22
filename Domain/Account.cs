64f65a1b06973d142851360efb464c5dd4d25b7e
xiangjiec <Xiang.jie@autumn.sg>
2024-09-22 19:00:54 +0800

    Add project files.

---
+++ Domain/Account.cs
@@ -0,0 +1,17 @@
+﻿namespace NRules.Samples.SimpleRules.Domain;
+
+public class Account
+{
+    public string AccountNumber { get; }
+    public Customer Owner { get; }
+    public bool IsActive { get; set; }
+    public bool IsDelinquent { get; set; }
+
+    public Account(string accountNumber, Customer owner)
+    {
+        AccountNumber = accountNumber;
+        Owner = owner;
+        IsActive = true;
+        IsDelinquent = false;
+    }
+}
---
+++ Domain/Customer.cs
@@ -0,0 +1,12 @@
+﻿namespace NRules.Samples.SimpleRules.Domain;
+
+public class Customer
+{
+    public string Name { get; }
+    public bool IsPreferred { get; set; }
+
+    public Customer(string name)
+    {
+        Name = name;
+    }
+}
